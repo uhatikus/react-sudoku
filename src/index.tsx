5a89a4a8eb21486a498b905a38dc4cb35e7ae1e4
Ukhatov Alexander <uhatov007@yandex.ru>
2022-12-16 23:27:54 +0300

    reformating everything

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 
 import { Content, Title, Card, Grid } from './components'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 
-const rootElement = document.getElementById('root') as HTMLElement;
-const root = createRoot(rootElement);
+const rootElement = document.getElementById('root') as HTMLElement
+const root = createRoot(rootElement)
 
 root.render(
-  <ThemeProvider theme = {theme}>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
     <Content data-cy="content">
       <Title data-cy="title">Test Sudoku</Title>
-      <Card data-cy="card"><Grid></Grid></Card>
+      <Card data-cy="card">
+        <Grid></Grid>
+      </Card>
     </Content>
   </ThemeProvider>
 )
