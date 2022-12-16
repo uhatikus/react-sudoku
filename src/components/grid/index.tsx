32df1365617ff12f5902e560634972e87738c4e3
Ukhatov Alexander <uhatov007@yandex.ru>
2022-12-17 01:12:52 +0300

    Now we can move within the grid

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,18 +1,71 @@
 import React, { FC, Children, useEffect, useCallback } from 'react'
-import { useDispatch } from 'react-redux'
+import useMousetrap from 'react-hook-mousetrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { AnyAction, Dispatch } from 'redux'
-import { createGrid } from '../../reducers'
-import { INDEX } from '../../typings'
+import { createGrid, IReducer, selectBlock } from '../../reducers'
+import { BLOCK_COORD, INDEX } from '../../typings'
 
 import Block from './block'
 import { Container, Row } from './styles'
 
+interface IState {
+  selectedBlock?: BLOCK_COORD
+}
 const Grid: FC = () => {
+  const state = useSelector<IReducer, IState>(({ selectedBlock }) => ({
+    selectedBlock,
+  }))
   const dispatch = useDispatch<Dispatch<AnyAction>>()
   const create = useCallback(() => dispatch(createGrid()), [dispatch])
   useEffect(() => {
     create()
   }, [create])
+
+  function moveDown() {
+    if (state.selectedBlock && state.selectedBlock[0] < 8) {
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] + 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      )
+    }
+  }
+  function moveLeft() {
+    if (state.selectedBlock && state.selectedBlock[1] > 0) {
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] - 1) as INDEX,
+        ])
+      )
+    }
+  }
+  function moveRight() {
+    if (state.selectedBlock && state.selectedBlock[1] < 8) {
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] + 1) as INDEX,
+        ])
+      )
+    }
+  }
+  function moveUp() {
+    if (state.selectedBlock && state.selectedBlock[0] > 0) {
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] - 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      )
+    }
+  }
+  useMousetrap('down', moveDown)
+  useMousetrap('left', moveLeft)
+  useMousetrap('right', moveRight)
+  useMousetrap('up', moveUp)
+
   return (
     <Container data-cy="grid-container">
       {Children.toArray(
