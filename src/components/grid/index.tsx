b77b613704cf464ad63f74f716728ba8c791a17e
Ukhatov Alexander <uhatov007@yandex.ru>
2022-12-19 00:03:05 +0300

    Now it's kinda working (but not really playable)

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -2,24 +2,39 @@ import React, { FC, Children, useEffect, useCallback } from 'react'
 import useMousetrap from 'react-hook-mousetrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { AnyAction, Dispatch } from 'redux'
-import { createGrid, IReducer, selectBlock } from '../../reducers'
-import { BLOCK_COORD, INDEX } from '../../typings'
+import { createGrid, fillBlock, IReducer, selectBlock } from '../../reducers'
+import { BLOCK_COORD, INDEX, NUMBERS, N } from '../../typings'
 
 import Block from './block'
 import { Container, Row } from './styles'
 
 interface IState {
   selectedBlock?: BLOCK_COORD
+  selectedValue?: N
 }
 const Grid: FC = () => {
-  const state = useSelector<IReducer, IState>(({ selectedBlock }) => ({
-    selectedBlock,
-  }))
+  const state = useSelector<IReducer, IState>(
+    ({ selectedBlock, workingGrid }) => ({
+      selectedBlock,
+      selectedValue:
+        workingGrid && selectedBlock
+          ? workingGrid[selectedBlock[0]][selectedBlock[1]]
+          : 0,
+    })
+  )
   const dispatch = useDispatch<Dispatch<AnyAction>>()
   const create = useCallback(() => dispatch(createGrid()), [dispatch])
   useEffect(() => {
     create()
   }, [create])
+
+  const fill = useCallback(
+    (n: NUMBERS) => {
+      if (state.selectedBlock && state.selectedValue === 0)
+        dispatch(fillBlock(n, state.selectedBlock))
+    },
+    [dispatch, state.selectedBlock, state.selectedValue]
+  )
 
   function moveDown() {
     if (state.selectedBlock && state.selectedBlock[0] < 8) {
@@ -61,6 +76,15 @@ const Grid: FC = () => {
       )
     }
   }
+  useMousetrap('1', () => fill(1))
+  useMousetrap('2', () => fill(2))
+  useMousetrap('3', () => fill(3))
+  useMousetrap('4', () => fill(4))
+  useMousetrap('5', () => fill(5))
+  useMousetrap('6', () => fill(6))
+  useMousetrap('7', () => fill(7))
+  useMousetrap('8', () => fill(8))
+  useMousetrap('9', () => fill(9))
   useMousetrap('down', moveDown)
   useMousetrap('left', moveLeft)
   useMousetrap('right', moveRight)
--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,6 +1,12 @@
 import { AnyAction } from 'redux'
+import { GRID } from '../typings'
 
-import { createFullGrid, removeNumbers, copyGrid } from '../utils'
+import {
+  createFullGrid,
+  removeNumbers,
+  copyGrid,
+  compareArrays,
+} from '../utils'
 
 import { IReducer } from './interfaces'
 import * as types from './types'
@@ -20,6 +26,22 @@ function reducer(state = initialState, action: AnyAction): IReducer {
         solvedGrid,
         workingGrid,
       }
+    }
+    case types.FILL_BLOCK: {
+      if (state.workingGrid && state.solvedGrid) {
+        if (
+          state.solvedGrid[action.coords[0]][action.coords[1]] != action.value
+        ) {
+          alert('Incorrect Option')
+          return state
+        }
+        state.workingGrid[action.coords[0]][action.coords[1]] = action.value
+        if (compareArrays(state.workingGrid, state.solvedGrid)) {
+          alert('Completed')
+          return { ...state, workingGrid: [...state.workingGrid] as GRID }
+        }
+      }
+      return state
     }
     case types.SELECT_BLOCK:
       return {
